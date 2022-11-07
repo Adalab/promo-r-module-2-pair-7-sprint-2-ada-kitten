505e00a941e89e414c09b92d03aaa6e4fe346ed1
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-07 21:29:45 +0100

    Clear inputs of add new kitten form

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-'use strict';
-/*VARIABLES*/ 
-const formElement = document.querySelector('.js-new-form');
-const dataElement = document.querySelector('.js-list');
-const input_search_desc = document.querySelector('.js_in_search_desc');
-const input_search_race = document.querySelector('.js_in_search_race');
-const menu = document.querySelector('.js-menu-nav');
-const button = document.querySelector('.js-btn-add');
-const buttonCancel = document.querySelector('.js-cancel');
-const buttonSearch = document.querySelector('.js-button-search');
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-race');
-const labelMesageError = document.querySelector('.js-label-error');
+"use strict";
+/*VARIABLES*/
+const formElement = document.querySelector(".js-new-form");
+const dataElement = document.querySelector(".js-list");
+const input_search_desc = document.querySelector(".js_in_search_desc");
+const input_search_race = document.querySelector(".js_in_search_race");
+const menu = document.querySelector(".js-menu-nav");
+const button = document.querySelector(".js-btn-add");
+const buttonCancel = document.querySelector(".js-cancel");
+const buttonSearch = document.querySelector(".js-button-search");
+const inputDesc = document.querySelector(".js-input-desc");
+const inputPhoto = document.querySelector(".js-input-photo");
+const inputName = document.querySelector(".js-input-name");
+const inputRace = document.querySelector(".js-input-race");
+const labelMesageError = document.querySelector(".js-label-error");
 
-/*VARIABLES*/ 
- 
+/*VARIABLES*/
+
 //Objeto para añadir gatitos
 
 const kittenDataList = [
-  { 
-    url: 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
-    name: 'Anastacio',
-    desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-    race: 'British Shorthair',
+  {
+    url: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+    name: "Anastacio",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
   },
   {
-    url: 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg',
-    name: 'Fiona',
-    desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-    race: 'British Shorthair',
+    url: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+    name: "Fiona",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
   },
-  { 
-    url: 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg',
-    name: 'Cielo',
-    desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-    race: 'British Shorthair',
+  {
+    url: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+    name: "Cielo",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
   },
-]
+];
 //formElement.classList.remove("collapsed");
 const kitten1Element = renderKitten(kittenDataList[0]);
 const kitten2Element = renderKitten(kittenDataList[1]);
@@ -46,25 +46,24 @@ dataElement.innerHTML = kitten1Element + kitten2Element + kitten3Element;
 
 //Esto es el filtro de busqueda, cuando vale '', luego en la función lo sustituyo por el valor del input (gato).
 
-//Estos son los dato variables, lo que va a ir dentor de nuestra función. Utilizamos datalElement (es el UL, el padre de las LI, los gatos) con innerHTML para pintar en la página la información (un gato). Despues con += le añadimos la función que hemos llamado antes (renderKitten) y en ella sustituimos los parametros específicos de dicja función (líneas de las 17 a la 40). 
-
+//Estos son los dato variables, lo que va a ir dentor de nuestra función. Utilizamos datalElement (es el UL, el padre de las LI, los gatos) con innerHTML para pintar en la página la información (un gato). Despues con += le añadimos la función que hemos llamado antes (renderKitten) y en ella sustituimos los parametros específicos de dicja función (líneas de las 17 a la 40).
 
 //-----------------------FUNCIONES----------------------------//
 //Esto quita el buscador
 function showNewCatForm(ev) {
   ev.preventDefault();
-  formElement.classList.remove('collapsed');
+  formElement.classList.remove("collapsed");
 }
 //Esto añade el buscador
 function hideNewCatForm(ev) {
   ev.preventDefault();
-  formElement.classList.add('collapsed');
+  formElement.classList.add("collapsed");
 }
 
 //Esto es la función manejadora
 function handleClickNewCatForm(event) {
   event.preventDefault();
-  if (formElement.classList.contains('collapsed')) {
+  if (formElement.classList.contains("collapsed")) {
     formElement.classList.remove("collapsed");
   } else {
     formElement.classList.add("collapsed");
@@ -73,17 +72,14 @@ function handleClickNewCatForm(event) {
 
 //Hemos creado una función para añadir gatitos, lo hacemos cuando clickamos en añadir. Para eso hemos tenido que crear un addEventListener (linea 133)
 function addNewKitten(event) {
-  event.preventDefault() 
+  event.preventDefault();
   const valueDesc = inputDesc.value;
   const valuePhoto = inputPhoto.value;
   const valueName = inputName.value;
 
-  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-    
-    labelMesageError.innerHTML = 'Debe rellenar todos los valores';
-
+  if (valueDesc === "" || valuePhoto === "" || valueName === "") {
+    labelMesageError.innerHTML = "Debe rellenar todos los valores";
   } else {
-
   }
 }
 
@@ -91,25 +87,30 @@ function addNewKitten(event) {
 
 const cancelNewKitten = (event) => {
   event.preventDefault();
-  formElement.classList.add('collapsed'); 
+  formElement.classList.add("collapsed");
+  inputDesc.value = "";
+  inputPhoto.value = "";
+  inputName.value = "";
+  inputRace.value = "";
 };
 
 //Para crear la función general de los gatos cogemos el ul (los li son los gatitos) y creamos el esqueleto con todos los elementos que lo contienen. Utilizamos las variables de forma neutra (sin contenido $(contenido)) para después cambiarlas con los datos que queramos (lineas de 50-58).
 
 function renderKitten(dataKitten) {
-  const kittenElement =  `<li class="card"> <article> <img class="card_img" src="${dataKitten.url}" alt="gatito"/><h3 class="card_title"> ${dataKitten.name}</h3><h4 class="card_race">${renderRace(dataKitten.race)}</h4><p class="card_description"> ${dataKitten.desc}</p></article></li>`;
+  const kittenElement = `<li class="card"> <article> <img class="card_img" src="${dataKitten.url}" alt="gatito"/><h3 class="card_title"> ${
+    dataKitten.name
+  }</h3><h4 class="card_race">${renderRace(dataKitten.race)}</h4><p class="card_description"> ${dataKitten.desc}</p></article></li>`;
 
   return kittenElement;
 }
 
-
 const filterKitten = (event) => {
   event.preventDefault();
-  dataElement.innerHTML = '';
+  dataElement.innerHTML = "";
   const descrSearchText = input_search_desc.value;
 
   if (kitten1Desc.includes(descrSearchText)) {
-      dataElement.innerHTML += renderKitten(kittenDataList[0]);
+    dataElement.innerHTML += renderKitten(kittenDataList[0]);
   }
   if (kitten2Desc.includes(descrSearchText)) {
     dataElement.innerHTML += renderKitten(kittenDataList[1]);
@@ -119,19 +120,13 @@ const filterKitten = (event) => {
   }
 };
 
-function renderRace (race){
-
-  if (race === '') {
-    
+function renderRace(race) {
+  if (race === "") {
     return '<p class="card_race">No se ha especificado la raza</p>';
-
   } else {
-
     return `<h3 class="card_race">${race}</h3>`;
-
   }
-
-};
+}
 
 //-----------------------FUNCIONES----------------------------//
 
@@ -148,19 +143,14 @@ function renderRace (race){
   
 });*/
 
-//Eventos que hacemos al clickar. 
+//Eventos que hacemos al clickar.
 
-button.addEventListener('click', addNewKitten);
+button.addEventListener("click", addNewKitten);
 
-buttonCancel.addEventListener('click', cancelNewKitten);
+buttonCancel.addEventListener("click", cancelNewKitten);
 
-buttonSearch.addEventListener('click', filterKitten);
+buttonSearch.addEventListener("click", filterKitten);
 
-menu.addEventListener('click', handleClickNewCatForm);
+menu.addEventListener("click", handleClickNewCatForm);
 
 //-----------------------EVENTOS----------------------------//
-
-
-
-
-
