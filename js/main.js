4a7f38362047315e0d616aecacc1376f178102b0
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-10 07:47:55 +0100

    Add exercise 2.12 filter kitten

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const inputName = document.querySelector(".js-input-name");
 const linkNewFormElememt = document.querySelector(".js-button-new-form");
 const labelMesageError = document.querySelector(".js-label-error");
 const input_search_desc = document.querySelector(".js_in_search_desc");
+const input_search_breed = document.querySelector(".js_in_search_breed");
 const addButton = document.querySelector(".js-add-button");
 
 //Objetos con cada gatito
@@ -26,14 +27,14 @@ const kittenData_2 = {
   image:
     "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
   name: "Fiona",
-  desc: "Juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  desc: "Cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
   breed: "British Shorthair",
 };
 const kittenData_3 = {
   image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
   name: "Cielo",
-  desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-  breed: "British Shorthair",
+  desc: "Dormilon, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  breed: "Persa",
 };
 
 const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
@@ -96,16 +97,16 @@ function addNewKitten(event) {
       desc: valueDesc,
       breed: valueBreed,
     };
-
+    //Añade el nuevo gato al array KittenDataList
     kittenDataList.push(newKittenDataObject);
-
+    //Limpia todos los values
     inputDesc.value = "";
     inputPhoto.value = "";
     inputName.value = "";
     inputBreed.value = "";
-
+    //Mostra el mensaje de que se creo el gato
     labelMesageError.innerHTML = "Mola! Un nuevo gatito en Adalab!";
-
+    //Vuelva a pintar la lista de gatos
     renderKittenList(kittenDataList);
   }
 }
@@ -124,13 +125,16 @@ function cancelNewKitten(event) {
 function filterKitten(event) {
   event.preventDefault();
   const descrSearchText = input_search_desc.value;
-
+  const breedSearchText = input_search_breed.value;
+  //Vacia primero
   listElement.innerHTML = "";
-  for (const kittenItem of kittenDataList) {
-    if (kittenItem.desc.includes(descrSearchText)) {
-      listElement.innerHTML += renderKitten(kittenItem);
-    }
-  }
+  //Filtra por descripción
+  const dataKittenFiltered = kittenDataList
+    .filter((kitten) => kitten.desc.toLowerCase().includes(descrSearchText.toLowerCase()))
+    //Filtrar por raza
+    .filter((kitten) => kitten.breed.toLowerCase().includes(breedSearchText.toLowerCase()));
+
+  renderKittenList(dataKittenFiltered);
 }
 
 //Mostrar el litado de gatitos en ell HTML
